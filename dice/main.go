57a2892305227bb4b073e94a112ffd2217ad7445
Sergey Katkalo <for_spam87@mail.ru>
2019-05-01 22:39:40 +0300

    Released "Advanced task" with dice frequency in percents

--- dice/main.go
+++ dice/main.go
@@ -16,12 +16,20 @@ func main() {
 
 	s := []int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 
+	frequency := make([]int, 11)
+
 	for i := 1; i <= tryCount; i++ {
 		t[diceValue()+diceValue()-2]++
 	}
 
 	o.PrintRow(s)
 	o.PrintRow(t)
+
+	for i, v := range t {
+		frequency[i] = v * 100 / tryCount
+	}
+
+	o.PrintRow(frequency)
 }
 
 func diceValue() int {

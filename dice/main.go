eae416e955db487628bb4138e155581e5a04802f
Sergey Katkalo <for_spam87@mail.ru>
2019-05-01 22:18:16 +0300

    Constants moved out from functions; constants changed to unexported for needs of our current project

--- dice/main.go
+++ dice/main.go
@@ -5,15 +5,18 @@ import (
 	o "dice/output"
 )
 
-func main(){
-	const TryCount = 100000
+const (
+	tryCount = 100000
+	maxValue = 6
+)
 
+func main(){
 	var t []int
 	t = make([]int, 11)
 	
 	s := [] int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 	
-	for i:=1; i<=TryCount; i++ {
+	for i:=1; i<=tryCount; i++ {
 		t[diceValue() + diceValue() - 2] ++
 	}
 
@@ -22,7 +25,5 @@ func main(){
 }
 
 func diceValue() int {
-	const MaxValue = 6
-	
-	return (r.Intn(MaxValue) + 1)
+	return (r.Intn(maxValue) + 1)
 }
